1a68e7dc0f326aa25bc126c26bfc85e333b03fde
Yassui <yassui.kimura@gmail.com>
2021-02-28 22:38:12 -0300

    feat: inicia intro python basics
    - var
    - print
    - len
    - input
    - functions
    - comentarios

---
+++ section-01/hello.py
@@ -0,0 +1,14 @@
+# This program says hello and asks for name and age
+
+print('Hello World!')
+print('What is your name?') # ask for their name
+myName = input()
+
+print('It is good to meet you, ' + myName)
+print('The length of your name is: ')
+print(len(myName))
+
+print('What is your age? ') # ask for their age
+myAge = input()
+print('You age will be ' + str(int(myAge) + 1) + ' in a year.')
+
